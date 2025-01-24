81f01020757e024ee4c4874e04aa44f79dca6d4e
Martin Rodriguez <martin.adolfo.rodriguez90@gmail.com>
2025-01-24 18:14:52 -0300

    update: invoices custom

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -13,19 +13,24 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
+const totalPendingInvoices = 0; // Define the variable
+const totalPaidInvoices = 0; // Define the variable
+const numberOfInvoices = 0; // Define the variable
+const numberOfCustomers = 0; // Define the variable
+
 export default async function CardWrapper() {
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
